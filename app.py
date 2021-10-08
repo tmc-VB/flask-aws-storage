12e0fca984dd66da19d5d4c4122522f471c19864
Theresa <30399299+tmc-VB@users.noreply.github.com>
2021-10-08 10:48:24 -0500

    changed s3 bucket back
    img-mgr-test-tmc

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 UPLOAD_FOLDER = "uploads"
-BUCKET = "cfngin-tmc-common-us-west-1"
+BUCKET = "img-mgr-test-tmc"
 
 @app.route("/")
 def home():
